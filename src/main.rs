433e6a962bfd66b156ed2c0fff498aa5d6c7e0f8
roberte777 <rewilkes0041@gmail.com>
2026-02-24 21:13:08 -0600

    basic iced setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{Column, button, column, text};
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+pub fn main() -> iced::Result {
+    iced::run(update, view)
+}
+
+fn update(value: &mut u64, message: Message) {
+    match message {
+        Message::Increment => *value += 1,
+    }
+}
+
+fn view(value: &u64) -> Column<'_, Message> {
+    column![text(value), button("+").on_press(Message::Increment),]
 }
